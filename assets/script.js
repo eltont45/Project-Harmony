60c01178fe0c5b180f7e5f48b6b4cd1d8bb277c4
Daniel Stephen <daniel.vergil2711@gmail.com>
2021-12-01 20:55:03 -0800

    Added API to fetch artist

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,31 @@
 var selectors = {
-    searchButton: document.querySelector('.search-btn')
+    searchField: document.querySelector('.search-artist'),
+    searchButton: document.querySelector('.search-btn'),
+    artistPageHeader: document.querySelector('.html2-header')
 };
 
-function loadArtist(){
+
+
+function loadArtist() {
     window.location.href = "./artist.html";
 }
-selectors.searchButton.addEventListener("click",loadArtist)
 
+function getArtist() {
+    var artistSearchText = selectors.searchField.value;
+    var myHeaders = new Headers();
+    myHeaders.append("x-rapidapi-host", "theaudiodb.p.rapidapi.com");
+    myHeaders.append("x-rapidapi-key", "e7e494b4d7msh0a6cfffe1539573p1a2d8bjsn45ebaf632221");
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch("https://theaudiodb.p.rapidapi.com/search.php?s="+artistSearchText, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+selectors.searchButton.addEventListener("click", getArtist)
